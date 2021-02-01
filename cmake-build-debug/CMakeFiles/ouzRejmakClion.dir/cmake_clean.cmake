c8c376e51263be1db923e12e4bad14e189342ce1
Jakub Rejmak <tokadorium@gmail.com>
2021-02-01 21:09:54 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ouzRejmakClion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ouzRejmakClion.dir/main.c.obj"
+  "libouzRejmakClion.dll.a"
+  "ouzRejmakClion.exe"
+  "ouzRejmakClion.exe.manifest"
+  "ouzRejmakClion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ouzRejmakClion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
