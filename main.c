394efaa145933566cc852cb88466347f972968b7
Jakub Rejmak <tokadorium@gmail.com>
2021-02-06 18:53:55 +0100

    Ouz2Commit1

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    char a[1000]={0};
+    int i=0, counter=0;
+
+    printf("Podaj tekst...\n");
+
+    while (1){
+        scanf("%c", &a[i]);
+        if ((a[i]=='\n')||i>1000){
+            break;
+        }
+        i++;
+    }
+
+    for (int j = 0; j <= i; ++j) {
+        if ((a[j]>=65 && a[j]<=90) || (a[j]>=97 && a[j]<=122)){
+            if (!((a[j+1]>=65 && a[j+1]<=90) || (a[j+1]>=97 && a[j+1]<=122))){
+                counter++;
+            }
+        }
+    }
+
+    for (int j = 0; j < i; ++j) {
+        printf("%c", a[i]);
+    }
+
+    printf("\n Counter: %d", counter);
+
     return 0;
 }
