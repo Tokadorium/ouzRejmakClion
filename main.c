1a3af0889702651593425d91127fecf7939d44f3
Jakub Rejmak <tokadorium@gmail.com>
2021-02-23 00:50:59 +0100

    recc

--- main.c
+++ main.c
@@ -1,85 +1,73 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
-const int rows=4, cols=4;
+int read_vector(int vec[], int size, int stop_value){
+    if(scanf("%d", vec)!=1){
+        return 1;
+    }
+    if(*vec==stop_value){
+        return 150-size;
+    }
+    return read_vector(vec+1, size-1, stop_value);
+}
 
-int main() {
-
-    int matrixA[rows][cols];
-    int matrixB[rows][cols];
-    int matrixC[rows][cols];
-
-//    info
-//    printf("boze\n");
-
-//    random data
-    srand(NULL);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            matrixA[i][j]=rand()%100;
-            matrixB[i][j]=rand()%100;
+int binary_search(const int arr[], int l, int r, int x){
+    int c=(l+r)/2;
+    if(l==r){
+        if(*(arr+l)==x){
+            return l;
+        }
+        else{
+            return -1;
         }
     }
-
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if (i==j){
-                matrixC[i][j]=1;
-            }
-            else{
-                matrixC[i][j]=0;
-            }
+    else {
+        if (x <= *(arr + c)) {
+            return binary_search(arr, l, c, x);
+        } else {
+            return binary_search(arr, c + 1, r, x);
         }
     }
+}
 
-//    user input
-//    for (int i = 0; i < rows; ++i) {
-//        for (int j = 0; j < cols; ++j) {
-//            if(scanf("%d", &matrix[i][j])!=1){
-//                printf("Incorrect input");
-//                return 1;
-//            }
-//        }
+int checkData(const int arr[], int size){
+    if(size<=1){
+        return 0;
+    }
+    if(*arr<*(arr+1)){
+        return checkData(arr+1, size-1);
+    }
+    else{
+        return 1;
+    }
+}
+
+int main(){
+    int arr[150];
+    int x;
+    printf("Input array: ");
+    int counter=read_vector(arr, 150, 0);
+    if(counter==1){
+        printf("Not enough data available");
+        return 3;
+    }
+    if(counter==149){
+        printf("Incorrect input");
+        return 1;
+    }
+//    for (int i = 0; i < counter; ++i) {
+//        printf("%d ", arr[i]);
 //    }
-
-//    swap diagonal numbers
-//    int temp;
-//    int colIndex=cols-1;
-//    for (int i = 0; i < rows; ++i) {
-//        temp=matrix[i][i];
-//        matrix[i][i]=matrix[i][colIndex];
-//        matrix[i][colIndex]=temp;
-//        colIndex--;
-//    }
-
-//    print matrix
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            printf("%d\t", matrixA[i][j]);
-            if (j!=0 && j%(rows-1)==0){
-                printf("\n");
-            }
-        }
+    printf("\n%d", counter);
+    fseek(stdin,0,SEEK_END);
+    printf("Input value to search: ");
+    if(scanf("%d", &x)!=1){
+        printf("Incorrect input");
+        return 1;
     }
-    printf("\n");
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            printf("%d\t", matrixB[i][j]);
-            if (j!=0 && j%(rows-1)==0){
-                printf("\n");
-            }
-        }
+    if(checkData(arr, counter)==1){
+        printf("Incorrect input data");
+        return 2;
     }
-    printf("\n");
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            printf("%d\t", matrixC[i][j]);
-            if (j!=0 && j%(rows-1)==0){
-                printf("\n");
-            }
-        }
-    }
-
+    printf("%d", binary_search(arr, 0, counter-1, x));
     return 0;
 }
